bd354afacb31b555e495aa08a7e5ec4d1a5270d2
Nknky09 <ks.shido@gmail.com>
2024-12-12 00:17:58 -0500

    v1.3.25 Landing.jsx css and Link

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`;
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <StyledBtn>styled btn</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span>
+          </h1>
+          <p>
+            I'm baby tote bag thundercats chambray grailed, franzen green juice
+            biodiesel seitan blog. Synth jean shorts thundercats portland yes
+            plz lomo. Semiotics offal DIY pop-up stumptown you probably haven't
+            heard of them austin thundercats hell of +1 truffaut poke.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
